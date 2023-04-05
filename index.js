2a308b26a75ed75a35a4034f72f42af9adbbfacb
Ashwin Pc <ashwinpc1993@gmail.com>
2023-04-04 21:35:39 -0700

    updates action

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+// Function to extract changelog entries from the PR description
+function extractChangelogEntries(prDescription) {
+  const changelogSection = prDescription.match(/## Changelog([\s\S]*?)##/);
+  if (changelogSection) {
+    const entries = changelogSection[1]
+      .trim()
+      .split("\n")
+      .map((entry) => entry.trim());
+    return entries;
+  }
+  return [];
+}
+
+// Define a mapping between the prefixes and section titles
+const prefixToSectionTitle = {
+  fix: "Bug Fixes",
+  feat: "Features",
+  unknown: "Uncategorized",
+  // Add more mappings if needed
+};
+
+function convertString(str, id, link) {
+  const prefixes = Object.keys(prefixToSectionTitle); // add other possible values as needed
+  const prefixeRegex = prefixes.join("|");
+  const regex = new RegExp(`-\\s(${prefixeRegex}):(.*)`);
+  const match = str.match(regex);
+  if (match) {
+    const [, prefix, text] = match;
+    return [`- ${text} ([#${id}](${link}))`, prefix];
+  }
+  return [str, "unknown"];
+}
+
 async function run() {
   try {
     // Read input parameters
@@ -24,6 +57,35 @@ async function run() {
 
     // Decode the content and add a new line
     const changelogContent = Buffer.from(fileData.content, "base64").toString();
+
+    // Extract the changelog entries from the PR description
+    const entries = extractChangelogEntries(context.payload.pull_request.body);
+
+    // If there are no entries, add a new entry with the PR title
+    if (entries.length === 0) {
+      entries.push(`- unknown: ${context.payload.pull_request.title}`);
+    }
+
+    // Update the changelog with the new entries
+    let updatedChangelogContent = changelogContent;
+    for (const entry of entries) {
+      const [entryText, prefix] = convertString(
+        entry,
+        pullRequestNumber,
+        context.payload.pull_request.html_url
+      );
+
+      const sectionTitle = prefixToSectionTitle[prefix[1].toLowerCase()];
+      const regex = new RegExp(
+        `(## Unreleased(?:[\\s\\S]*?)## ${sectionTitle}(?:[\\s\\S]*?))(\\n[^#]|$)`,
+        "i"
+      );
+      updatedChangelogContent = updatedChangelogContent.replace(
+        regex,
+        `$1\n${entryText}\n$2`
+      );
+    }
+
     const newChangelogContent = `${changelogContent}\n- PR #${pullRequestNumber}: ${context.payload.pull_request.title}`;
 
     // Update the changelog file
