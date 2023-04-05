025edee24a97e41ff46505484d2326ef8d2527fb
Ashwin Pc <ashwinpc1993@gmail.com>
2023-04-04 19:34:59 -0700

    updates actions

--- index.js
+++ index.js
@@ -3,55 +3,44 @@ const github = require("@actions/github");
 
 async function run() {
   try {
-    // Get the pull request title
-    const title = github.context.payload.pull_request.title;
-    console.log(`The pull request title is: ${title}`);
+    // Read input parameters
+    const token = process.env.INPUT_TOKEN;
+    const changelogPath = process.env.INPUT_CHANGELOG_PATH;
 
-    // get the repos changelog file
-    const changelog = await github.repos.getContents({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      path: "CHANGELOG.md",
+    // Set up Octokit with the provided token
+    const octokit = github.getOctokit(token);
+
+    // Get context data
+    const context = github.context;
+    const { owner, repo } = context.repo;
+    const pullRequestNumber = context.payload.pull_request.number;
+
+    // Get the content of the changelog file
+    const { data: fileData } = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path: changelogPath,
     });
 
-    // update the changelog file with a changelog entry
-    const newChangelog = changelog.data + " - " + title;
-    await github.repos.createOrUpdateFile({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      path: "CHANGELOG.md",
-      message: "Update changelog",
-      content: Buffer.from(newChangelog).toString("base64"),
-      sha: changelog.data.sha,
+    // Decode the content and add a new line
+    const changelogContent = Buffer.from(fileData.content, "base64").toString();
+    const newChangelogContent = `${changelogContent}\n- PR #${pullRequestNumber}: ${context.payload.pull_request.title}`;
+
+    // Update the changelog file
+    await octokit.rest.repos.createOrUpdateFileContents({
+      owner,
+      repo,
+      path: changelogPath,
+      message: `Update CHANGELOG.md for PR #${pullRequestNumber}`,
+      content: Buffer.from(newChangelogContent).toString("base64"),
+      sha: fileData.sha,
+      branch: context.payload.pull_request.head.ref,
     });
 
-    // create a commit with the updated changelog
-    const commit = await github.git.createCommit({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      message: "Update changelog",
-      tree: github.context.payload.pull_request.head.sha,
-      parents: [github.context.payload.pull_request.head.sha],
-    });
-
-    // update the pull request with the new commit
-    await github.git.updateRef({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      ref: "heads/" + github.context.payload.pull_request.head.ref,
-      sha: commit.data.sha,
-    });
-
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput('who-to-greet');
-    // console.log(`Hello ${nameToGreet}!`);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    console.log("Changelog file updated successfully.");
   } catch (error) {
-    core.setFailed(error.message);
+    console.error(`Error updating changelog: ${error}`);
+    process.exit(1);
   }
 }
 
